07459028bbd3c8ac3aed81f6584fdc693681cad2
Ravi <ravichem06@outlook.com>
2016-09-18 01:38:35 +0530

    Listing create

---
+++ app/assets/javascripts/react-libraries.js
@@ -0,0 +1,5 @@
+//React Library
+React = require('react');
+//Material Design Library
+MaterialUi = require('material-ui');
+injectTapEventPlugin = require('react-tap-event-plugin'); injectTapEventPlugin();
