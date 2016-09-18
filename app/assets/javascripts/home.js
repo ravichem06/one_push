9eb15cb7f0817f061b4a364141de328fd3d2fb1f
Ravi <ravichem06@outlook.com>
2016-09-18 16:45:57 +0530

    Few tweaks made

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,3 +6,12 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+	$('.Contribute').hide();
+	$('.search-result').delay(10000).hide(0,function(){
+		$('.Contribute').show();
+	});
+	
+	$('.modal-trigger').leanModal();
+});
